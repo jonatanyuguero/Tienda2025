0fc8f051de073015f6ed569c98bd9853f9ed20af
jonatanys <OGQ6920@educastur.es>
2025-02-04 13:42:49 +0100

    Tienda2025 commit 1

---
+++ Tienda2025/src/main/java/org/educastur/ogq69520/tienda2025/Tienda2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.educastur.ogq69520.tienda2025;
+
+/**
+ *
+ * @author alu30d
+ */
+public class Tienda2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
